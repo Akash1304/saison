7c88a6a6e1a732c21f6aa863163668449e2d3313
akash.panigrahi <akash.panigrahi1@oyorooms.com>
2022-11-20 15:22:18 +0530

    base code setup done

---
+++ src/main/java/com/akash/saison/SaisonApplication.java
@@ -0,0 +1,13 @@
+package com.akash.saison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaisonApplication.class, args);
+	}
+
+}
